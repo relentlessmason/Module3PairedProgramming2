27cc1cbff0a76d2938da88eaba8c750c04f3abd1
Mason <astinch09@gmail.com>
2022-07-27 19:20:45 -0400

    got problem to add +1

--- js/app.js
+++ js/app.js
@@ -1,6 +1,12 @@
-    let currentScore = 0;
-    const limit = 10;
+
+const limit = 10;
+var gameover = false;
     let answer = 0;
+    var currentProblem = 0;
+    var currentScore = 0;
+    var displayProb = document.querySelector("#currentProblem");
+    var displayScore = document.querySelector("#currentScore");
+
 
     function generate_equation(){
         const prob1 = Math.floor(Math.random() * 10) + 1;
@@ -11,13 +17,11 @@
         const dummyAnswer2 = Math.floor(Math.random() * 50);
         const dummyAnswer3 = Math.floor(Math.random() * 50);
 
-        const opt1 = document.getElementById('opt1'),
-        opt2 = document.getElementById('opt2'),
-        opt3 = document.getElementById('opt3'),
+        const opt1 = document.getElementById('opt1');
+        opt2 = document.getElementById('opt2');
+        opt3 = document.getElementById('opt3');
         opt4 = document.getElementById('opt4');
         currentScore = document.getElementById('currentScore');
-        
-
 
         allAnswer = [];
         switchAnswers = [];
@@ -29,52 +33,68 @@
 
         for (i = allAnswer.length; i--;){
             switchAnswers.push(allAnswer.splice(Math.floor(Math.random() * (i + 1)), 1)[0]);
-        }
+        };
 
         opt1.innerHTML = switchAnswers[0];
         opt2.innerHTML = switchAnswers[1];
         opt3.innerHTML = switchAnswers[2];
         opt4.innerHTML = switchAnswers[3];
 
-    };
+    }; //end of generate_equation
 
-    opt1.addEventListener("click", function() {
-        if(opt1.innerHTML === answer) {
+    opt1.addEventListener("click", ()=> {
+        if(opt1.innerHTML == answer) {
+            currentScore = currentScore + 1;
+            currentProblem = currentProblem + 1;
+            displayProb.textContent = currentProblem;
+            currentScore.textContent = currentScore;
             generate_equation();
-            currentScore.innerHTML = currentScore++;
         }
         else {
             generate_equation();
+            currentProblem = currentProblem + 1;
         }
     });
 
-    opt2.addEventListener("click", function(){
-        if(opt2.innerHTML === answer) {
+    opt2.addEventListener("click", ()=> {
+        if(opt2.innerHTML == answer) {
+            currentScore = currentScore + 1;
+            currentProblem = currentProblem + 1;
+            displayProb.textContent = currentProblem;
+            currentScore.textContent = currentScore;
             generate_equation();
-            currentScore.innerHTML = currentScore++;
         }
         else {
             generate_equation();
+            currentProblem = currentProblem + 1;
         }
     });
 
-    opt3.addEventListener("click", function(){
+    opt3.addEventListener("click", ()=> {
         if(opt3.innerHTML == answer) {
+            currentScore = currentScore + 1;
+            currentProblem = currentProblem + 1;
+            displayProb.textContent = currentProblem;
+            currentScore.textContent = currentScore;
             generate_equation();
-            currentScore.innerHTML = currentScore++;
         }
         else {
             generate_equation();
+            currentProblem = currentProblem + 1;
         }
     });
 
-    opt4.addEventListener("click", function(){
-        if(opt4.innerHTML === answer) {
+    opt4.addEventListener("click", ()=> {
+        if(opt4.innerHTML == answer) {
+            currentScore = currentScore + 1;
+            currentProblem = currentProblem + 1;
+            displayProb.textContent = currentProblem;
+            currentScore.textContent = currentScore;
             generate_equation();
-            currentScore.innerHTML = currentScore++;
         }
         else {
             generate_equation();
+            currentProblem = currentProblem + 1;
         }
     });
 
